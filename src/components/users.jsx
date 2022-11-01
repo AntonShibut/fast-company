d7ce1b3e40b905cb6e9423774aa76eb5d415ad27
Anton Shhibut <messenger5251@gmail.com>
2022-11-01 20:07:42 +0300

    fix const renderPhrase

--- src/components/users.jsx
+++ src/components/users.jsx
@@ -13,14 +13,12 @@ const Users = () => {
         }
 
     const renderPhrase = (number) => {
-        let numbToStr = toString(number)
-        return (number <= 4 && number >= 2) ? `${number} человека тусанут с тобой` :
-        (number === 0) ? 'Никто с тобой не тусанёт':
-        `${number} человек тусанёт с тобой` 
+        const lastOne = Number(number.toString().slice(-1))
+        if (number > 4 && number < 15) return 'человек тусанет'
+        if ([2,3,4].indexOf(lastOne) >= 0) return 'человека тусанут'
+        if (lastOne === 1) return 'человек тусанет'
     }     
-         
-        
-    
+            
 
     const classForPhrase = () => {
         let classOfPhrase = 'badge m-2 '
@@ -70,22 +68,30 @@ const Users = () => {
 
     return (
         <>
-            <span className={classForPhrase()}>{renderPhrase(users.length)}</span>
-            <table className="table">
-                <thead>
-                    <tr>
-                    <th>Имя</th>
-                    <th> Качества</th>
-                    <th> Профессия</th>
-                    <th> Встретился, раз</th>
-                    <th> Оценка</th>
-                    <th> </th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {tableRow()}
-                </tbody>
-            </table>
+            <h2><span className={classForPhrase()}>
+                    {users.length > 0
+                        ? `${users.length} ${renderPhrase(users.length)} с тобой сегодня`
+                        : 'Никто с тобой не тусанет'
+                    }
+                </span>
+            </h2>
+            {users.length > 0 && (
+                <table className="table">
+                    <thead>
+                        <tr>
+                        <th>Имя</th>
+                        <th> Качества</th>
+                        <th> Профессия</th>
+                        <th> Встретился, раз</th>
+                        <th> Оценка</th>
+                        <th> </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tableRow()}
+                    </tbody>
+                </table>
+            )}
         </> 
         
     )
